cde4a4fcc47992c95de801fb5e2d27c53ac59a05
Jacob Barta <jacob.barta25@gmail.com>
2023-06-04 15:48:40 -0400

    convert to react native (kind of)

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,51 @@
+import {Linking, StyleSheet} from 'react-native';
+
+import EditScreenInfo from '../../components/EditScreenInfo';
+import { Text, View } from '../../components/Themed';
+import QRCodeScanner from "react-native-qrcode-scanner";
+import {RNCamera} from 'react-native-camera';
+
+export default function TabOneScreen() {
+
+  let onSuccess = (e:any) => {
+    if(e.data) {
+      console.log(e.data)
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab One</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="app/(tabs)/index.tsx" />
+
+      <RNCamera
+                // ref={ref => {
+                //   camera = ref;
+                // }}
+                style={{
+                    flex: 1,
+                  width: '100%',
+                }}
+              >
+              </RNCamera>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
